00c3fcca81bf76764bd497618cf2dede76606be6
farrellw <farrell.will.g@gmail.com>
2020-04-18 13:09:53 -0500

    Add clickable US map which prints out the name of the state you clicked on.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
 import * as React from "react";
-import Body from "./lib/Body";
-import Header from "./lib/Header";
 import "./App.scss";
-import { useLocation } from "react-router-dom";
-import schoolExpenses from "./data/SchoolExpenses.json";
-import { ISchool } from "./models/Data";
+import "ol/ol.css";
+import * as OLKit from "@bayer/ol-kit";
 
-function useQuery() {
-  return new URLSearchParams(useLocation().search);
-}
-
-const schoolExpensesDictionary = {};
-schoolExpenses.forEach(school => {
-  schoolExpensesDictionary[school.id] = school;
-});
+import { createUSStatesLayer } from "./utils";
 
 function App() {
-  const query = useQuery();
-  const selectedIds: string[] = query.getAll("id");
-  const selectedSchools: ISchool[] = selectedIds.map(
-    id => schoolExpensesDictionary[id]
-  );
-
-  return (
-    <>
-      <Header schools={selectedSchools} />
-      <Body schools={selectedSchools} />
-    </>
-  );
+  const onMapInit = (map: any) => {
+    map.on("click", (event: any) => {
+      map.forEachFeatureAtPixel(event.pixel, function (
+        feature: any,
+        layer: any
+      ) {
+        const p = feature.getProperties()
+        if (p) {
+          console.log(p["NAME"]);
+          console.log(layer)
+        }
+      });
+    });
+    const layer = createUSStatesLayer();
+    map.addLayer(layer);
+  };
+  return <OLKit.Map onMapInit={onMapInit} />;
 }
 
 export default App;
