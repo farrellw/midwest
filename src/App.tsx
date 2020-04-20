f4901e98fcd2dae1317023d686b32bdfe67be2e4
farrellw <farrell.will.g@gmail.com>
2020-04-20 13:50:25 -0500

    Submit map with form.

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,62 @@ import * as React from "react";
 import "./App.scss";
 import "ol/ol.css";
 import * as OLKit from "@bayer/ol-kit";
-
+import Feature from 'ol/feature';
+import Map from 'ol/Map';
 import { createUSStatesLayer } from "./utils";
 
 function App() {
-  const onMapInit = (map: any) => {
+  var formData: {
+    features: Feature[]
+  } = { features: [] };
+  const onMapInit = (map: Map) => {
+    const layer = createUSStatesLayer();
     map.on("click", (event: any) => {
-      map.forEachFeatureAtPixel(event.pixel, function (
-        feature: any
-      ) {
-        const p = feature.getProperties()
+      map.forEachFeatureAtPixel(event.pixel, function (feature: any) {
+        const p = feature.getProperties();
         if (p) {
           const selected = !p.selected;
           feature.setProperties({ ...p, selected: selected });
         }
+        const features = layer.getSource().getFeatures();
+        formData.features = features;
       });
     });
-    const layer = createUSStatesLayer();
-    map.addLayer(layer);  
+    map.addLayer(layer);
     OLKit.centerAndZoom(map, {
       x: -98.5795,
       y: 39.8283,
-      zoom: 4
-    })
+      zoom: 4,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    console.log(formData);
+    e.preventDefault();
   };
 
   return (
-    <section className="map-container">
-      <OLKit.Map onMapInit={onMapInit}>
-        <OLKit.Controls />
-        <OLKit.Popup />
-      </OLKit.Map>
-    </section>
+    <div className="app-page">
+      <section className="header">
+        <h3>What states belong to the 'Midwest'?</h3>
+        <p>
+          Click the appropriate states you think belong in the midwest. If a
+          state is in red, that means you think it is part of the "Midwest".
+          When you are done, submit your picks at the bottom.
+        </p>
+      </section>
+      <section className="map-container">
+        <OLKit.Map onMapInit={onMapInit}>
+          <OLKit.Controls />
+          <OLKit.Popup />
+        </OLKit.Map>
+      </section>
+      <section className="submission-section">
+        <form onSubmit={handleSubmit}>
+          <input type="submit" value="Submit" />
+        </form>
+      </section>
+    </div>
   );
 }
 
--- src/Router.tsx
+++ src/Router.tsx
@@ -5,10 +5,21 @@ import { BrowserRouter, Route } from "react-router-dom";
 function Router() {
   return (
     <BrowserRouter>
-      <Route path="*">
+      <Route path="/">
         <App />
       </Route>
+      <Route path="/results">
+        <Results />
+      </Route>
     </BrowserRouter>
+  );
+}
+
+function Results() {
+  return (
+    <div>
+      <h2>Results</h2>
+    </div>
   );
 }
 
